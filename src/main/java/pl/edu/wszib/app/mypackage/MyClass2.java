b8d04f6c6dde77c4258f369642bcc2f76f2d96dd
Kamil Marcin Kasprzyk <kakasprb@student.wszib.edu.pl>
2022-10-02 15:16:52 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/app/mypackage/MyClass2.java
@@ -0,0 +1,16 @@
+package pl.edu.wszib.app.mypackage;
+
+import pl.edu.wszib.app.MyClass;
+//  pl.edu.wszib.app.mypackage.MyClass
+//  pl.edu.wszib.app.MyClass
+
+public class MyClass2 extends MyClass {
+
+//    public MyClass myClass;
+
+    public void run(){
+        field3 = "Field3 from extended";
+        run3();
+//        myClass.run4();
+    }
+}
